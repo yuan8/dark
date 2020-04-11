fbdb8835bdc11865a948f3664c84cfd46649ed87
yuan8 <bhalayuan@gmail.com>
2020-04-11 21:08:57 +0700

    jja

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,71 +17,71 @@ class AppServiceProvider extends ServiceProvider
     {
        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
 
-         $event->menu->add([
-               'text' => 'Dashboard',
-               'icon'=>'fa fa-home',
-               'url'=>url('/')
+         // $event->menu->add([
+         //       'text' => 'Dashboard',
+         //       'icon'=>'fa fa-home',
+         //       'url'=>url('/')
 
-           ]);
+         //   ]);
 
-           $event->menu->add([
-                 'text' => 'Pelaporan',
-                 'icon'=>'fa fa-file',
-                 'submenu'=>[
-                  [
-                    'text'=>'Chart',
-                    'url'=>route('pel')
-                  ],
-                  [
-                    'text'=>'Map',
-                    'url'=>route('pel.map')
-                  ]
-                 ]
-             ]);
-             $event->menu->add([
-                   'text' => 'Realisasi Tingkat Pusat',
-                   'icon'=>'fa fa-chart-bar',
-                   'submenu'=>[
-                     [
-                       'text'=>'Rekap Nasional',
-                       'url'=>route('rel.nas')
-
-
-                     ],
-                     [
-                        'text'=>'Rekap Perbidang',
-                        'url'=>route('rel.nas.bidang')
-
-                     ]
-
-                   ]
-
-               ]);
-           $event->menu->add([
-                 'text' => 'Realisasi Tingkat Daerah',
-                 'icon'=>'fa fa-chart-area',
-                 'submenu'=>[
-                   [
-                     'text'=>'Tingkat Provinsi',
-                     'url'=>route('rel.daerah.pro')
+         //   $event->menu->add([
+         //         'text' => 'Pelaporan',
+         //         'icon'=>'fa fa-file',
+         //         'submenu'=>[
+         //          [
+         //            'text'=>'Chart',
+         //            'url'=>route('pel')
+         //          ],
+         //          [
+         //            'text'=>'Map',
+         //            'url'=>route('pel.map')
+         //          ]
+         //         ]
+         //     ]);
+         //     $event->menu->add([
+         //           'text' => 'Realisasi Tingkat Pusat',
+         //           'icon'=>'fa fa-chart-bar',
+         //           'submenu'=>[
+         //             [
+         //               'text'=>'Rekap Nasional',
+         //               'url'=>route('rel.nas')
 
 
-                   ],
-                   [
-                      'text'=>'Tingkat Daerah',
-                      'url'=>route('rel.daerah.kota')
+         //             ],
+         //             [
+         //                'text'=>'Rekap Perbidang',
+         //                'url'=>route('rel.nas.bidang')
 
-                   ]
+         //             ]
 
-                 ]
+         //           ]
 
-             ]);
-             $event->menu->add([
-                   'text' => 'Pilih Tahun',
-                   'icon'=>'fa fa-calender',
-                   'url'=>route('f.pilih_tahun')
+         //       ]);
+         //   $event->menu->add([
+         //         'text' => 'Realisasi Tingkat Daerah',
+         //         'icon'=>'fa fa-chart-area',
+         //         'submenu'=>[
+         //           [
+         //             'text'=>'Tingkat Provinsi',
+         //             'url'=>route('rel.daerah.pro')
 
-               ]);
+
+         //           ],
+         //           [
+         //              'text'=>'Tingkat Daerah',
+         //              'url'=>route('rel.daerah.kota')
+
+         //           ]
+
+         //         ]
+
+         //     ]);
+         //     $event->menu->add([
+         //           'text' => 'Pilih Tahun',
+         //           'icon'=>'fa fa-calender',
+         //           'url'=>route('f.pilih_tahun')
+
+         //       ]);
 
 
        });
